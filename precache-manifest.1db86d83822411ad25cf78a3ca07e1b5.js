675b6651de6ef38ed3f7fe959874416dc5128a8c
Pankaj Kumar <805.bluebell@gmail.com>
2019-03-27 18:52:27 +0530

    Updates

---
+++ precache-manifest.1db86d83822411ad25cf78a3ca07e1b5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "052fad23ca9f036fcf47",
+    "url": "/react-todo/static/js/runtime~main.79666361.js"
+  },
+  {
+    "revision": "95ace8f4af79d2d6d3e9",
+    "url": "/react-todo/static/js/main.6a50f3e7.chunk.js"
+  },
+  {
+    "revision": "2822e1e6695fbf0ae41b",
+    "url": "/react-todo/static/js/2.44b20be8.chunk.js"
+  },
+  {
+    "revision": "95ace8f4af79d2d6d3e9",
+    "url": "/react-todo/static/css/main.550211d3.chunk.css"
+  },
+  {
+    "revision": "60a734048065f87da9a5c4d293738eb6",
+    "url": "/react-todo/index.html"
+  }
+];
